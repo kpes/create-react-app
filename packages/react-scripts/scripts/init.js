a770de6463d90ae35da00f87838ffe9d0a66c3fe
Keith Pesavento <kpes@users.noreply.github.com>
2019-02-16 16:59:55 -0500

    Start of work to create template that includes Electron

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -19,7 +19,6 @@ const path = require('path');
 const chalk = require('react-dev-utils/chalk');
 const execSync = require('child_process').execSync;
 const spawn = require('react-dev-utils/crossSpawn');
-const { defaultBrowsers } = require('react-dev-utils/browsersHelper');
 const os = require('os');
 const verifyTypeScriptSetup = require('./utils/verifyTypeScriptSetup');
 
@@ -85,6 +84,9 @@ module.exports = function(
   const ownPath = path.dirname(
     require.resolve(path.join(__dirname, '..', 'package.json'))
   );
+  const ownPackage = require(path.join(
+    path.join(__dirname, '..', 'package.json')
+  ));
   const appPackage = require(path.join(appPath, 'package.json'));
   const useYarn = fs.existsSync(path.join(appPath, 'yarn.lock'));
 
@@ -95,10 +97,10 @@ module.exports = function(
 
   // Setup the script rules
   appPackage.scripts = {
-    start: 'react-scripts start',
-    build: 'react-scripts build',
-    test: 'react-scripts test',
-    eject: 'react-scripts eject',
+    start: `${ownPackage['name']} start`,
+    build: `${ownPackage['name']} build`,
+    test: `${ownPackage['name']} test`,
+    eject: `${ownPackage['name']} eject`,
   };
 
   // Setup the eslint config
@@ -107,7 +109,7 @@ module.exports = function(
   };
 
   // Setup the browsers list
-  appPackage.browserslist = defaultBrowsers;
+  appPackage.browserslist = ['Electron'];
 
   fs.writeFileSync(
     path.join(appPath, 'package.json'),
@@ -164,7 +166,7 @@ module.exports = function(
     command = 'npm';
     args = ['install', '--save', verbose && '--verbose'].filter(e => e);
   }
-  args.push('react', 'react-dom');
+  args.push('react', 'react-dom', 'electron');
 
   // Install additional template dependencies, if present
   const templateDependenciesPath = path.join(
@@ -184,7 +186,7 @@ module.exports = function(
   // Install react and react-dom for backward compatibility with old CRA cli
   // which doesn't install react and react-dom along with react-scripts
   // or template is presetend (via --internal-testing-template)
-  if (!isReactInstalled(appPackage) || template) {
+  if (!areDependenciesInstalled(appPackage) || template) {
     console.log(`Installing react and react-dom using ${command}...`);
     console.log();
 
@@ -258,11 +260,12 @@ module.exports = function(
   console.log('Happy hacking!');
 };
 
-function isReactInstalled(appPackage) {
+function areDependenciesInstalled(appPackage) {
   const dependencies = appPackage.dependencies || {};
 
   return (
     typeof dependencies.react !== 'undefined' &&
-    typeof dependencies['react-dom'] !== 'undefined'
+    typeof dependencies['react-dom'] !== 'undefined' &&
+    typeof dependencies['electron'] !== 'undefined'
   );
 }
